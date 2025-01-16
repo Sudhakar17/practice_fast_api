38653d09f6109381019d36312ce5381a7385d6c5
Sudhakar17 <sudhakarece17@gmail.com>
2025-01-16 17:37:46 +0100

    format the code

--- 07_body_multiple_params/main.py
+++ 07_body_multiple_params/main.py
@@ -8,6 +8,7 @@ from typing import Annotated
 
 app = FastAPI()
 
+
 class Item(BaseModel):
     name: str
     description: str | None = None
@@ -19,9 +20,13 @@ class User(BaseModel):
     username: str
     full_name: str | None = None
 
+
 @app.put("/items/{item_id}")
-async def update_item( *,
+async def update_item(
+    *,
     item_id: int = Path(..., title="The ID of the item to get", gt=10, le=100),
-    item: Item | None, user: User | None):
+    item: Item | None,
+    user: User | None
+):
     results = {"item_id": item_id, "item": item, "user": user}
     return results
