51a69311d235162af3ac0f8c983bbebd1a011a4d
Sudhakar17 <sudhakarece17@gmail.com>
2025-01-16 17:04:20 +0100

    update body multiple params

---
+++ 07_body_multiple_params/main.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+from fastapi import FastAPI, Query, Path, Body
+from pydantic import BaseModel
+
+from typing import Annotated
+
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+class User(BaseModel):
+    username: str
+    full_name: str | None = None
+
+@app.put("/items/{item_id}")
+async def update_item( *,
+    item_id: int = Path(..., title="The ID of the item to get", gt=10, le=100),
+    item: Item | None, user: User | None):
+    results = {"item_id": item_id, "item": item, "user": user}
+    return results
